9188f5ad2d1332ab14775e28ab776179e68016f9
Javier <jachavez6@gmail.com>
2024-02-01 21:23:11 -0500

    Write simple API wrapper to access WMATA rail station API

--- simplemetro/apps/core/utils.py
+++ simplemetro/apps/core/utils.py
@@ -12,4 +12,29 @@ def lines():
 
     return lines
 
+class Result:
+    def __init__(self, status_code: int, message: str = '', data:dict[str, list] = None):
+        self.status_code = int(status_code)
+        self.message = str(message)
+        self.data = data if data else []
+
+class RestAdapter:
+    def __init__(self,
+                 host: str = 'api.wmata.com',
+                 api_key: str = os.environ['WMATA_KEY'],
+                 api: str = 'Rail.svc',
+                 json: str = 'json',
+                ):
+        self.url = 'https://{}/{}/{}/'.format(host, api, json)
+        self._api_key = api_key
+        self.api = api
+
+    def get(self, endpoint: str) -> dict[str, list]:
+        full_url = self.url + endpoint
+        headers = {'api_key': self._api_key}
+        response = requests.get(url=full_url, headers=headers)
+        data_out = response.json()
+        if response.status_code >= 200 and response.status_code <= 299: # OK
+            return data_out
+        raise Exception(data_out['message']) # Todo: raise custom exception later
 
--- simplemetro/apps/core/views.py
+++ simplemetro/apps/core/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import TemplateView
 
-from .utils import lines
+from .utils import RestAdapter
 class IndexView(TemplateView):
     template_name = 'core/index.html'
     extra_context = {'title': 'Simple Metro'}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['lines'] = lines()
+        trains = RestAdapter()
+        context['lines'] = trains.get('jLines')
         return context
