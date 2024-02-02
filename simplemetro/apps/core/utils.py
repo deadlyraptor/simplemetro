ac3064bc4f346cfbb6a611525b8fcf26a0363b6b
Javier <jachavez6@gmail.com>
2024-02-01 22:50:25 -0500

    Abstract wrapper to return Result object

--- simplemetro/apps/core/utils.py
+++ simplemetro/apps/core/utils.py
@@ -12,29 +12,58 @@ def lines():
 
     return lines
 
+class Line:
+    def __init__(self, 
+                 line: str,
+                 display_name: str,
+                 start_station: str,
+                 end_station: str,
+                 internal_destination_one: str = '',
+                 internal_destination_two: str = ''):
+        self.line = line
+        self.display_name = display_name
+        self.start_station = start_station
+        self.end_station = end_station
+        self.internal_destination_one = internal_destination_one
+        self.internal_destination_two = internal_destination_two
+        
 class Result:
     def __init__(self, status_code: int, message: str = '', data:dict[str, list] = None):
+        """Result returned from low-level RestAdapter
+
+        :param status_code: standard HTTP status code
+        :param message: Human readable result
+        :param data: Python List of Dictionaries
+        """
         self.status_code = int(status_code)
         self.message = str(message)
         self.data = data if data else []
 
-class RestAdapter:
+class Metro:
+    """
+    1. Instantiate the Metro class
+    2. Call Metro.get_lines()...
+    3. ...which returns Metro.get()...
+    4. ...which returns the Result object...
+    5. ...which contains the API response in the data parameter
+    """
     def __init__(self,
-                 host: str = 'api.wmata.com',
+                 base_url: str = 'api.wmata.com',
                  api_key: str = os.environ['WMATA_KEY'],
                  api: str = 'Rail.svc',
-                 json: str = 'json',
+                 response_format: str = 'json',
                 ):
-        self.url = 'https://{}/{}/{}/'.format(host, api, json)
+        self.url = 'https://{}/{}/{}/'.format(base_url, api, response_format)
         self._api_key = api_key
         self.api = api
 
-    def get(self, endpoint: str) -> dict[str, list]:
+    def get(self, endpoint: str) -> Result:
         full_url = self.url + endpoint
         headers = {'api_key': self._api_key}
         response = requests.get(url=full_url, headers=headers)
         data_out = response.json()
-        if response.status_code >= 200 and response.status_code <= 299: # OK
-            return data_out
-        raise Exception(data_out['message']) # Todo: raise custom exception later
+        return Result(response.status_code, message=response.reason, data=data_out)
+
+    def get_lines(self):
+        return self.get('jLines')
 
--- simplemetro/apps/core/views.py
+++ simplemetro/apps/core/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView
 
-from .utils import RestAdapter
+from .utils import Metro
+
+trains = Metro()
 class IndexView(TemplateView):
     template_name = 'core/index.html'
     extra_context = {'title': 'Simple Metro'}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        trains = RestAdapter()
-        context['lines'] = trains.get('jLines')
+        context['lines'] = trains.get_lines().data
         return context
